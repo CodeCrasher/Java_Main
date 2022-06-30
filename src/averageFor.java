80d22c9a3fb2c3b60f24a4bd393a39db915e4f88
Ambhoj Shukla <ambhojshukla@192-168-1-157.tpgi.com.au>
2022-06-30 23:16:17 +1000

    Average with for loop_formatted

--- src/averageFor.java
+++ src/averageFor.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class averageFor {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int total=0,average,counter,grade;
+        int total = 0, average, counter, grade;
         System.out.println("Enter the frequency of numbers you need the average of :");
         int num = scn.nextInt();
-        for(counter=0;counter<num;counter++){
-            System.out.printf("Enter the %d number :",counter+1);
-            grade=scn.nextInt();
-            total=total+grade;
+        for (counter = 0; counter < num; counter++) {
+            System.out.printf("Enter the %d number :", counter + 1);
+            grade = scn.nextInt();
+            total = total + grade;
         }
-        average=total/num;
-        System.out.printf("The average of %d numbers is : %d",num,average);
+        average = total / num;
+        System.out.printf("The average of %d numbers is : %d", num, average);
     }
 }
