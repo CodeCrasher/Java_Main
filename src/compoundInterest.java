303e9d9b0a3767bb8e8aeff8367167103951afee
Ambhoj Shukla <ambhojshukla@192-168-1-157.tpgi.com.au>
2022-06-30 23:48:40 +1000

    Compound Interest

---
+++ src/compoundInterest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class compoundInterest {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double amount, principal, rate;
+        int year;
+        System.out.println("Enter the year for the Compound Interest:");
+        year = scn.nextInt();
+        System.out.println("Enter the principal for the Compound Interest:");
+        principal = scn.nextDouble();
+        System.out.println("Enter the rate for the Compound Interest:");
+        rate = scn.nextDouble();
+        for (int i = 0; i <= year; i++) {
+            amount = principal * Math.pow(1 + rate, year);
+            System.out.printf("The compound interest is %.2f for %d year.\n", amount, year);
+        }
+
+    }
+}
