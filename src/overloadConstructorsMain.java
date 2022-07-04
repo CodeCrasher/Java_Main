7779d301e0ce58d4f7a440b9be43fc1852eb52ec
Ambhoj Shukla <ambhojshukla@Ambhojs-MBP.gateway>
2022-07-04 20:15:43 +1000

    Multiple objects with multiple constructors

--- src/overloadConstructorsMain.java
+++ src/overloadConstructorsMain.java
@@ -1,5 +1,13 @@
 public class overloadConstructorsMain {
     public static void main(String[] args) {
+        overloadConstructors oc = new overloadConstructors();
+        overloadConstructors oc_one = new overloadConstructors(5);
+        overloadConstructors oc_two = new overloadConstructors(5, 3);
+        overloadConstructors oc_three = new overloadConstructors(5, 3, 8);
 
+        System.out.printf("%s\n", oc.toDisplayMilitary());
+        System.out.printf("%s\n", oc_one.toDisplayMilitary());
+        System.out.printf("%s\n", oc_two.toDisplayMilitary());
+        System.out.printf("%s\n", oc_three.toDisplayMilitary());
     }
 }
