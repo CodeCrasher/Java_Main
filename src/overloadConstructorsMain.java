70f818b3157b73beb275b204bfb0dffae519d4e1
Ambhoj Shukla <ambhojshukla@Ambhojs-MBP.gateway>
2022-07-04 20:19:45 +1000

    Multiple objects with multiple constructors || Refined a little

--- src/overloadConstructorsMain.java
+++ src/overloadConstructorsMain.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class overloadConstructorsMain {
     public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int hh, mm, ss;
+        System.out.print("Enter hours:");
+        hh = scn.nextInt();
+        System.out.print("Enter minutes:");
+        mm = scn.nextInt();
+        System.out.print("Enter seconds:");
+        ss = scn.nextInt();
+
         overloadConstructors oc = new overloadConstructors();
-        overloadConstructors oc_one = new overloadConstructors(5);
-        overloadConstructors oc_two = new overloadConstructors(5, 3);
-        overloadConstructors oc_three = new overloadConstructors(5, 3, 8);
+        overloadConstructors oc_one = new overloadConstructors(hh);
+        overloadConstructors oc_two = new overloadConstructors(hh, mm);
+        overloadConstructors oc_three = new overloadConstructors(hh,mm,ss);
 
         System.out.printf("%s\n", oc.toDisplayMilitary());
         System.out.printf("%s\n", oc_one.toDisplayMilitary());
