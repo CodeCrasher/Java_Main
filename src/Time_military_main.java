77b21f270f1b0b0eaca3461e7ffc9ff7645195be
Ambhoj Shukla <ambhojshukla@Ambhojs-MBP.gateway>
2022-07-04 19:35:50 +1000

    Time in both military and regular with minor other changes

--- src/Time_military_main.java
+++ src/Time_military_main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Time_military_main {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int hh, mm, ss;
+        System.out.print("Enter hours:");
+        hh = scn.nextInt();
+        System.out.print("Enter minutes:");
+        mm = scn.nextInt();
+        System.out.print("Enter seconds:");
+        ss = scn.nextInt();
         time_military tm = new time_military();
-        tm.setTime(19,17,4);
+        tm.setTime(hh, mm, ss);
         System.out.println(tm.toMilitaryTime());
+        System.out.println(tm.toRegularTime());
     }
 }
--- src/time_military.java
+++ src/time_military.java
@@ -3,13 +3,17 @@ public class time_military {
     private int minute;
     private int second;
 
-    public void setTime(int h,int m,int s){
-        hour = ((h>=0&&h<24) ? h : 0);
-        minute = ((m>=0&&m<60) ? m : 0);
-        second = ((s>=0&&s<60) ? s : 0);
+    public void setTime(int h, int m, int s) {
+        hour = ((h >= 0 && h < 24) ? h : 0);
+        minute = ((m >= 0 && m < 60) ? m : 0);
+        second = ((s >= 0 && s < 60) ? s : 0);
     }
 
-    public String toMilitaryTime(){
-        return String.format("%02d:%02d:%02d",hour,minute,second);
+    public String toMilitaryTime() {
+        return String.format("Time in military format = %02d:%02d:%02d", hour, minute, second);
+    }
+
+    public String toRegularTime() {
+        return String.format("Time in regular format = %d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour % 12), minute, second, ((hour < 12) ? "AM" : "PM"));
     }
 }
