f51a4742e5f2adad2ee322bbe6e5224de6f13810
Ambhoj Shukla <ambhojshukla@192-168-1-157.tpgi.com.au>
2022-07-05 17:43:16 +1000

    Composition|| Redefined

--- src/toStringMain.java
+++ src/toStringMain.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class toStringMain {
     public static void main(String[] args) {
-        toStringTute ts = new toStringTute(27, 02, 1997);
-        Composition c = new Composition("AJ",ts);
+
+        Scanner scn = new Scanner(System.in);
+
+        String Name;
+        int day, month, year;
+        System.out.println("Enter your name:");
+        Name = scn.next();
+        System.out.println("Enter day:");
+        day = scn.nextInt();
+        System.out.println("Enter Month:");
+        month = scn.nextInt();
+        System.out.println("Enter year:");
+        year = scn.nextInt();
+        toStringTute ts = new toStringTute(day, month, year);
+        Composition c = new Composition(Name, ts);
 
         System.out.println(c);
     }
