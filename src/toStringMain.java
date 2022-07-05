3c04dba83dced92a5a569cf46fd6e8cb3579eea9
Ambhoj Shukla <ambhojshukla@192-168-1-157.tpgi.com.au>
2022-07-05 17:35:46 +1000

    Composition with toString format

--- src/toStringMain.java
+++ src/toStringMain.java
@@ -1,5 +1,8 @@
 public class toStringMain {
     public static void main(String[] args) {
         toStringTute ts = new toStringTute(27, 02, 1997);
+        Composition c = new Composition("AJ",ts);
+
+        System.out.println(c);
     }
 }
