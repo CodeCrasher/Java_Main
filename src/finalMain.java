4354e2130cee2287be4bfdff6a48e0fbea590c86
Ambhoj Shukla <ambhojshukla@192-168-1-157.tpgi.com.au>
2022-07-08 19:24:55 +1000

    Final constants in java

---
+++ src/finalMain.java
@@ -0,0 +1,9 @@
+public class finalMain {
+    public static void main(String[] args) {
+        finalTute ft = new finalTute(10);
+        for (int i = 0; i < 5; i++) {
+            ft.add();
+            System.out.printf("%s", ft);
+        }
+    }
+}
