ac91e4e1b93a4e676f265e02e6c8bd2cbb479677
Ambhoj Shukla <ambhojshukla@Ambhojs-MBP.gateway>
2022-07-11 20:14:40 +1000

    Polymorphism main arguments class

---
+++ src/polyArgMain.java
@@ -0,0 +1,8 @@
+public class polyArgMain {
+    public static void main(String[] args) {
+        PolyArguments pa = new PolyArguments();
+        polymorphismSuper v = new polymorphismSuper();
+
+        pa.takeValue(v);
+    }
+}
