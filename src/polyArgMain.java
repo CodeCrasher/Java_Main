339a7302197061253bdacc8236a7b95665015be6
Ambhoj Shukla <ambhojshukla@Ambhojs-MBP.gateway>
2022-07-11 20:17:48 +1000

    Poly Args final

--- src/polyArgMain.java
+++ src/polyArgMain.java
@@ -2,7 +2,11 @@ public class polyArgMain {
     public static void main(String[] args) {
         PolyArguments pa = new PolyArguments();
         polymorphismSuper v = new polymorphismSuper();
+        polymorphismSuper vmin = new polymorphismMin();
+        polymorphismSuper vmax = new polymorphismMax();
 
         pa.takeValue(v);
+        pa.takeValue(vmin);
+        pa.takeValue(vmax);
     }
 }
