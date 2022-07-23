e4afa5c0f007f02499a0e29c11a5c178b74a7500
Ambhoj Shukla <ambhojshukla@Ambhojs-MBP.gateway>
2022-07-23 21:22:28 +1000

    Class holding objects

---
+++ src/dogListClassObjectAdd.java
@@ -0,0 +1,8 @@
+public class dogListClassObjectAdd {
+    public static void main(String[] args) {
+        dogList dlo = new dogList();
+        dog dO = new dog();
+        dlo.add(dO);
+        dlo.add(dO);
+    }
+}
