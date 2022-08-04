417d2ab691a5d2e822a92c14eb04b21db801cb3a
Ambhoj Shukla <ambhojshukla@Ambhojs-MBP.gateway>
2022-08-04 11:12:55 +1000

    Final jButton

--- src/guiJb.java
+++ src/guiJb.java
@@ -1,6 +1,7 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Handler;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
@@ -8,11 +9,11 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 
-public class guiJb extends JFrame{
+public class guiJb extends JFrame {
     private JButton reg;
     private JButton custom;
 
-    public guiJb(){
+    public guiJb() {
         super("The title");
         setLayout(new FlowLayout());
 
@@ -22,10 +23,21 @@ public class guiJb extends JFrame{
         Icon b = new ImageIcon(getClass().getResource("b.png"));
         Icon x = new ImageIcon(getClass().getResource("x.png"));
 
-        custom = new JButton("Custom",b);
+        custom = new JButton("Custom", b);
         custom.setRolloverIcon(x);
         add(custom);
 
+        HandlerClass handler = new HandlerClass();
+        reg.addActionListener(handler);
+        custom.addActionListener(handler);
+
+
+    }
+
+    private class HandlerClass implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            JOptionPane.showMessageDialog(null, String.format("%s", event.getActionCommand()));
+        }
     }
 
 
--- src/guiJbMain.java
+++ src/guiJbMain.java
@@ -1,2 +1,10 @@
+import javax.swing.JFrame;
+
 public class guiJbMain {
+    public static void main(String[] args) {
+        guiJb go = new guiJb();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(500, 500);
+        go.setVisible(true);
+    }
 }
